e59fc0caf15f14ba8d673799825711b5248683ec
ShahrukhAzeem <shahrukh.2790@gmail.com>
2021-10-21 00:43:29 +0500

    change in app js

--- src/app.js
+++ src/app.js
@@ -15,40 +15,40 @@ app.use(express.urlencoded({
 
 app.use(cors())
 
-db.sequelize.sync()
-// db.sequelize.sync({ force: true })
-//   .then(function () {
-//     const currentDate = new Date();
-//     const dateString = currentDate.toISOString()
-//     const hotels = [];
-//     for(let i = 0; i < 10; i++) {
-//       hotels.push(
-//         { id: i+1, name: `${randomWords()}_hotel`, created_at: dateString }
-//       )
-//     }
+// db.sequelize.sync()
+db.sequelize.sync({ force: true })
+  .then(function () {
+    const currentDate = new Date();
+    const dateString = currentDate.toISOString()
+    const hotels = [];
+    for(let i = 0; i < 10; i++) {
+      hotels.push(
+        { id: i+1, name: `${randomWords()}_hotel`, created_at: dateString }
+      )
+    }
 
-//     db.Hotel.bulkCreate(hotels)
+    db.Hotel.bulkCreate(hotels)
 
-//     const reviews = []
-//     for(let i = 0; i < 50; i++) {
-//       const hotel_id = Math.floor(Math.random() * 5) + 1;
-//       const score = Math.floor(Math.random() * 5) + 1;
-//       const comment = randomWords({ exactly: 5, join: ' ' });
-//       const year = Math.floor(Math.random() * 2) + 2019;
-//       const month = Math.floor(Math.random() * 12) + 1;
-//       const date = Math.floor(Math.random() * 28) + 1;
-//       const created_date = `${year}-${month}-${date}`
-//       reviews.push({
-//         id: i+1, hotel_id, score, comment, created_date
-//       })
-//     }
+    const reviews = []
+    for(let i = 0; i < 50; i++) {
+      const hotel_id = Math.floor(Math.random() * 5) + 1;
+      const score = Math.floor(Math.random() * 5) + 1;
+      const comment = randomWords({ exactly: 5, join: ' ' });
+      const year = Math.floor(Math.random() * 2) + 2019;
+      const month = Math.floor(Math.random() * 12) + 1;
+      const date = Math.floor(Math.random() * 28) + 1;
+      const created_date = `${year}-${month}-${date}`
+      reviews.push({
+        id: i+1, hotel_id, score, comment, created_date
+      })
+    }
 
-//     db.Review.bulkCreate(reviews)
+    db.Review.bulkCreate(reviews)
 
-//   })
-//   .then(function() {
-//     console.log("Seed data has been inserted in DB")
-//   })
+  })
+  .then(function() {
+    console.log("Seed data has been inserted in DB")
+  })
 
 app.use('/', indexRouter);
 
